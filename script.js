ba3d4d38753ba441cf80a11c7105ea059c802b5a
Kiridepapel <monalek0622@hotmail.com>
2023-06-07 18:45:19 -0500

    main: base v5

---
+++ script.js
@@ -0,0 +1,48 @@
+/* !===== NO TOCAR (BACKGROUND) ======! */
+// BACKGROUND
+new BackgroundAnimation({
+  count: 300,
+  size: {
+    min: 2,
+    max: 8,
+    pulse: 0,
+  },
+  speed: {
+    x: {
+      min: 0,
+      max: 0.4,
+    },
+    y: {
+      min: 0,
+      max: 0.6,
+    },
+  },
+  colors: {
+    // background: "#1e1c2a",
+    background: "#000000",
+    particles: ["#2ec98e", "#ffb75c", "#2ec98e"],
+  },
+  blending: "screen",
+  opacity: {
+    center: 0.3,
+    edge: 0,
+  },
+  skew: 0,
+  shapes: ["s"],
+});
+
+/* ?===== HEADER: MAGIPO ======? */
+
+/* ?===== HOME: MAGIPO ======? */
+
+/* ?===== HOME: ANA MARIA ======? */
+
+/* ?===== ABOUT US: BRIAN ======? */
+
+/* ?===== CONTACT: HITALO ======? */
+
+/* ?===== LOGIN: WILLY ======? */
+
+/* ?===== REGISTER: WILLY ======? */
+
+/* ?===== FOOTER: MAGIPO =====? */
