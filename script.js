2259dc8d8dbd39889c02db938c7c0908a1f8aec3
Kiridepapel <monalek0622@hotmail.com>
2023-06-07 20:18:23 -0500

    Squashed commit of the following:
    commit d5952620bceea95b83a4f8ff04305e8726b603e4
    Author: Kiridepapel <monalek0622@hotmail.com>
    Date:   Wed Jun 7 20:16:31 2023 -0500
    
        si
    
    commit a72fbe285e349c3c3e8c75eb3906f4074e9c11df
    Merge: c7ae233 5e43926
    Author: Brian Uceda <89280866+Kiridepapel@users.noreply.github.com>
    Date:   Wed Jun 7 20:14:22 2023 -0500
    
        Merge pull request #3 from NUTRITIONAL-PRIORITY/features
    
        Features

---
+++ script.js
@@ -0,0 +1,92 @@
+/* !===== NO TOCAR (BACKGROUND) ======! */
+// BACKGROUND
+new BackgroundAnimation({
+  count: 300,
+  size: {
+    min: 2,
+    max: 8,
+    pulse: 0,
+  },
+  speed: {
+    x: {
+      min: 0,
+      max: 0.4,
+    },
+    y: {
+      min: 0,
+      max: 0.6,
+    },
+  },
+  colors: {
+    // background: "#1e1c2a",
+    background: "#000000",
+    particles: ["#2ec98e", "#ffb75c", "#2ec98e"],
+  },
+  blending: "screen",
+  opacity: {
+    center: 0.3,
+    edge: 0,
+  },
+  skew: 0,
+  shapes: ["s"],
+});
+
+/* ?===== HEADER: MAGIPO ======? */
+// -------------------> MOSTRAR / OCULTAR ITEMS AL DARLE CLICK AL MENU DE HAMBURGUESA
+let burguer = document.querySelector(".bars");
+let navList = document.querySelector(".nav-list");
+burguer.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+
+// -------------------> PINTA DE OTRO COLOR EL ITEM QUE COINCIDA CON LA RUTA DEL PATH ACTUAL
+// Obtiene el path actual de la página
+let currentPage = window.location.pathname.split("/").pop(); // Obtiene el path actual del usuario: "Path: /localhost/index.html" -> "Obtiene: index.html"
+let navItems = document.querySelectorAll(".nav-list .nav-item");
+let navButtons = document.querySelectorAll(".login-register .button_1");
+
+// Agrega la clase "active" al item que coincida con el path actual
+navItems.forEach((item) => {
+  let itemLink = item.querySelector(".nav-list .nav-item a");
+  let href = itemLink.getAttribute("href").split("/").pop(); // Obtiene el path de cada <a>
+  if (href === currentPage) {
+    item.classList.add("active"); // <li>
+    itemLink.classList.add("active"); // <a>
+  }
+});
+
+// Agrega la clase "active" al botón correspondiente a la página actual
+navButtons.forEach((item) => {
+  let href = item.getAttribute("href").split("/").pop();
+  if (href === currentPage) {
+    item.classList.add("active");
+  }
+});
+
+// -------------------> HACER TRANSPARENTE Y DE COLOR EL NAVBAR CUANDO LLEGA A TOP = 0
+// Obtener el elemento del navbar
+let navTransparent = document.getElementsByClassName("navbar-container")[0];
+// Función para cambiar el color del navbar
+function changeNavbarColor() {
+  if (window.scrollY > 0) {
+    navTransparent.classList.add("navbar_color");
+  } else {
+    navTransparent.classList.remove("navbar_color");
+  }
+}
+// Agregar un event listener para detectar el scroll
+window.addEventListener("scroll", changeNavbarColor);
+
+/* ?===== HOME: MAGIPO ======? */
+
+/* ?===== HOME: ANA MARIA ======? */
+
+/* ?===== ABOUT US: BRIAN ======? */
+
+/* ?===== CONTACT: HITALO ======? */
+
+/* ?===== LOGIN: WILLY ======? */
+
+/* ?===== REGISTER: WILLY ======? */
+
+/* ?===== FOOTER: MAGIPO =====? */
